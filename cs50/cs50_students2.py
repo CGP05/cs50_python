b325dad9cb34e925812631a378a2862b6253ed7f
Christian <christian_pasquali@yahoo.com>
2026-06-02 18:25:41 -0400

    fixed typos

--- cs50/cs50_students2.py
+++ cs50/cs50_students2.py
@@ -2,28 +2,31 @@ class Politician:
     def __init__(self, name, party):
         if not name:
             raise ValueError("Missing name")
-        if party not in ["Liberal", "Conservative", "Bloc", "New Democratic"]:
+        if party not in ["Liberal", "Conservative", "Bloc Quebecois", "New Democratic"]:
             raise ValueError("Invalid party")
         self.name = name
         self.party = party
-        
+
+
     def __str__(self):
         return f"MP {self.name} from {self.party} party"
     
     def ideology(self):
         match self.party:
             case "Liberal":
-                return " centrist liberalism"
+                return "centrist liberalism"
             case "Conservative":
                 return "right-wing populism"
-            case "Bloc":
-                return "Quebec seperatism"
-            case "NDP":
+            case "Bloc Quebecois":
+                return "Quebec separatism"
+            case "New Democratic":
                 return "democratic socialism"
 
 def main():
     politician = get_politician()
+    politician.party = "24 Sussex Drive"
     print(politician)
+
 
 def get_politician():
     name = input("Name: ")
