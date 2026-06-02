6c565fc103e692b28d45a383d1180642cf4dee31
Christian <christian_pasquali@yahoo.com>
2026-06-02 13:06:53 -0400

    changed class from Student to Politician

--- cs50/cs50_students2.py
+++ cs50/cs50_students2.py
@@ -1,23 +1,25 @@
-class Student:
-    def __init__(self, name, party):
+class Politician:
+    def __init__(self, name, party, slogan):
         if not name:
             raise ValueError("Missing name")
         if party not in ["Liberal", "Conservative", "Bloc", "NDP"]:
             raise ValueError("Invalid party")
         self.name = name
         self.party = party
+        self.slogan = slogan
 
     def __str__(self):
         return f"{self.name} from {self.party}"
 
 def main():
-    student = get_student()
-    print(student)
+    politician = get_politician()
+    print(politician)
 
-def get_student():
+def get_politician():
     name = input("Name: ")
     party = input("Party: ")
-    return Student(name, party)
+    slogan = input("Slogan:")
+    return Politician(name, party, slogan)
 
 if __name__ == "__main__":
     main()
