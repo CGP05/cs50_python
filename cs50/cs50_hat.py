212f6856ca477d4fcd7d442dc7a7a64e923710b5
Christian <christian_pasquali@yahoo.com>
2026-06-09 13:22:50 -0400

    added note about where lists of constants should be

--- cs50/cs50_hat.py
+++ cs50/cs50_hat.py
@@ -3,6 +3,7 @@ import random
 class Tie: 
     def __init__(self):
         self.parties = ["Liberal", "Conservative", "Bloc", "New Democratic", "Green"]
+        #generally put lists of constants at top of file or class (like this)
     
     def sort(self, name):
         print(name, "is in", random.choice(self.parties))
