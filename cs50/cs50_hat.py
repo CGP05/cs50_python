f671dc42cd694df933f1ed8255218bf93c7e314b
Christian <christian_pasquali@yahoo.com>
2026-06-09 13:20:18 -0400

    random library

--- cs50/cs50_hat.py
+++ cs50/cs50_hat.py
@@ -1,6 +1,12 @@
+import random
+
 class Tie: 
+    def __init__(self):
+        self.parties = ["Liberal", "Conservative", "Bloc", "New Democratic", "Green"]
+    
     def sort(self, name):
-        print(name, "is in", "some party")
+        print(name, "is in", random.choice
+
 
 tie = Tie()
 tie.sort("Carney")
