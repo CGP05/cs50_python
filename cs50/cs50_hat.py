a5c65429250141b9f1ebb4fe5043277609d1ce61
Christian <christian_pasquali@yahoo.com>
2026-06-10 13:11:12 -0400

    made sentence printed gramatically correct

--- cs50/cs50_hat.py
+++ cs50/cs50_hat.py
@@ -1,12 +1,13 @@
 import random
 
+
 class Tie: 
     def __init__(self):
-        self.parties = ["Liberal", "Conservative", "Bloc", "New Democratic", "Green"]
+        self.parties = ["Liberal", "Conservative", "Bloc Québécois", "New Democratic", "Green"]
         #generally put lists of constants at top of file or class (like this)
     
     def sort(self, name):
-        print(name, "is in", random.choice(self.parties))
+        print(name, "is a member of", random.choice(self.parties), "party.")
 
 
 tie = Tie()
