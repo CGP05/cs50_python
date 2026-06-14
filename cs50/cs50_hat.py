575d186cd6f2e180816eed4d16db8182ba0eb9e0
Christian <christian_pasquali@yahoo.com>
2026-06-14 13:49:12 -0400

    added @classmethod to def sort

--- cs50/cs50_hat.py
+++ cs50/cs50_hat.py
@@ -5,7 +5,8 @@ class Tie:
     parties = ["Liberal", "Conservative", "Bloc Québécois", "New Democratic", "Green"]
         #generally put lists of constants at top of file or class (like this)
     
-    def sort(self, name):
+    @classmethod
+    def sort(cls, name):
         print(name, "is a member of", random.choice(self.parties), "party.")
 
 
