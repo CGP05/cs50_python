9329e69474d39ad26ae1274a5a6285b6d1d0c8b9
Christian <christian_pasquali@yahoo.com>
2026-06-14 13:32:25 -0400

    indented parties variable under class Tie

--- cs50/cs50_hat.py
+++ cs50/cs50_hat.py
@@ -2,7 +2,7 @@ import random
 
 
 class Tie: 
-parties = ["Liberal", "Conservative", "Bloc Québécois", "New Democratic", "Green"]
+    parties = ["Liberal", "Conservative", "Bloc Québécois", "New Democratic", "Green"]
         #generally put lists of constants at top of file or class (like this)
     
     def sort(self, name):
