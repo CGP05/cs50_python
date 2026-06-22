57716225b108348e430cfd225146b04e3a1d09ad
Christian <christian_pasquali@yahoo.com>
2026-06-22 14:47:34 -0400

    added super().__init__(name) to Backbencher and PartyLeader

--- cs50/cs50_wizzard.py
+++ cs50/cs50_wizzard.py
@@ -6,6 +6,7 @@ class MP:
 
 class Backbencher(MP):
     def __init__(self, name, party):
+        super().__init__(name)
         self.party = party
 
     ...
@@ -13,6 +14,7 @@ class Backbencher(MP):
 
 class PartyLeader(MP):
     def __init__(self, name, leader_since):
+        super().__init__(name)
         self.leader_since = leader_since
 
     ...
