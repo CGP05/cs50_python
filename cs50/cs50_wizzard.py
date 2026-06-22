df7800621a2dd4fecdd0fe61f3c05c76f1c0075e
Christian <christian_pasquali@yahoo.com>
2026-06-22 10:55:06 -0400

    new

---
+++ cs50/cs50_wizzard.py
@@ -0,0 +1,14 @@
+class Politician:
+    def __init__(self, name, party):
+        self.name = name
+        self.party = party
+
+    ...
+
+
+class PartyLeader:
+    def __init__(self, name, riding):
+        self.name = name
+        self.riding = riding
+
+    ...
