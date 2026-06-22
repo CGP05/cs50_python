71fded1a4949dc9fb24ece4185c1c6260d68e5f1
Christian <christian_pasquali@yahoo.com>
2026-06-22 14:32:53 -0400

    removed error checking from Backbencher and PartyLeader, so its only in class MP

--- cs50/cs50_wizzard.py
+++ cs50/cs50_wizzard.py
@@ -1,13 +1,11 @@
 class MP:
     def __init__(self, name):
         if not name:
-            raise ValueError
+            raise ValueError("Missing name")
         self.name = name
 
 class Backbencher:
     def __init__(self, name, party):
-        if not name:
-            raise ValueError("Missing name")
         self.name = name
         self.party = party
 
@@ -16,8 +14,6 @@ class Backbencher:
 
 class PartyLeader:
     def __init__(self, name, leader_since):
-        if not name:
-            raise ValueError("Missing name")
         self.name = name
         self.leader_since = leader_since
 
