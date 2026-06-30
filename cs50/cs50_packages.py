d0dce6f29b14f8757fe3fe10322bef396d75b200
Christian <christian_pasquali@yahoo.com>
2026-06-30 19:26:37 -0400

    replaced weight with fish_type

--- cs50/cs50_packages.py
+++ cs50/cs50_packages.py
@@ -1,13 +1,13 @@
 class Package:
-    def __init__(self, number, sender, recipient, weight):
+    def __init__(self, number, sender, recipient, fish_type):
         self.number = number
         self.sender = sender
         self.recipient = recipient
-        self.weight = weight
+        self.fish_type = fish_type
 
 def main():
     packages = [
-        
+        Package(number=1, sender="Kotaro", recipient="Aty",fish_type= )
     ]
 
 main()
