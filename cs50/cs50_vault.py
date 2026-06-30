cd7a82b4515c7ad78ca7508fe24a771470463ad0
Christian <christian_pasquali@yahoo.com>
2026-06-29 20:43:24 -0400

    cannot directly add the totals of the 2 banks together

--- cs50/cs50_vault.py
+++ cs50/cs50_vault.py
@@ -14,9 +14,7 @@ print(bank_of_canada)
 bank_of_togo = Vault(25, 50, 100)
 print(bank_of_togo)
 
-gold = bank_of_canada.gold + bank_of_togo.gold
-silver = bank_of_canada.silver + bank_of_togo.silver
-bronze = bank_of_canada.bronze + bank_of_togo.bronze
 
-total = Vault(gold, silver, bronze)
+
+total = bank_of_canada + bank_of_togo
 print(total)
