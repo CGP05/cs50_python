2e4c5f0b0fdc526f613072829c13855c5a0692fd
Christian <christian_pasquali@yahoo.com>
2026-06-30 19:15:33 -0400

    otters delivering packages

--- cs50/cs50_food.py
+++ cs50/cs50_food.py
@@ -1,9 +1,11 @@
 class Food:
     def __init__(self, ingredients):
         self.ingredients = ingredients
+        self.hearts = 
 
 
 def main():
+    mushroom_skewer = Food(ingredients=["Mushroom", "Hearty Mushroom"])
     ...
 
 main()
---
+++ cs50/cs50_packages.py
@@ -0,0 +1,4 @@
+def main():
+    packages = ["Package 1: Kotaro to Hana, 10kg", "Package 2: Hana to Ui, 5kg"]
+
+main()
