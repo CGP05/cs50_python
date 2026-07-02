31ab31a2a1917767d03c47faf42f3c6a05c7a736
Christian <christian_pasquali@yahoo.com>
2026-07-02 15:42:35 -0400

    update recipient names in main function for package instances

--- cs50/cs50_shortpackages.py
+++ cs50/cs50_shortpackages.py
@@ -7,7 +7,7 @@ class Package:
 
 def main():
     packages = [
-        Package(number=1, sender="Alice", recipient="Bob", weight=2.5),
-        Package(number=2, sender="Charlie", recipient="David", weight=1.2)
+        Package(number=1, sender="Alice", recipient="DeeDee", weight=2.5),
+        Package(number=2, sender="DeeDee", recipient="Hana", weight=1.2)
         
     ]
