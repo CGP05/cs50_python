6510b90b45900503cc497e70c5dbac46e47a6c70
Christian <christian_pasquali@yahoo.com>
2026-07-01 21:50:04 -0400

    added print in main()

--- cs50/cs50_food.py
+++ cs50/cs50_food.py
@@ -13,6 +13,6 @@ class Food:
 
 def main():
     mushroom_skewer = Food(ingredients=["Mushroom", "Hearty Mushroom"])
-    ...
+    print(f"This skewer heals {mushroom_skewer.hearts} hearts!")
 
 main()
