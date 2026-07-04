92a6fc9253cd15685d857ce3c9868ace3efcf935
Christian <christian_pasquali@yahoo.com>
2026-07-03 21:37:14 -0400

    redo bank with class Account (modelling with oop)

--- cs50/cs50_bank.py
+++ cs50/cs50_bank.py
@@ -1,22 +1,7 @@
-balance = 0
+class Account:
+    def __init__(self):
+        self._balance = 0
 
-
-def main():
-    print("Balance:", balance)
-    deposit(100)
-    withdraw(50)
-    print("Balance:", balance)
-
-
-def deposit(n):
-    global balance
-    balance += n
-
-
-def withdraw(n):
-    global balance
-    balance -= n
-
-
-if __name__ == "__main__":
-    main()
+    @property
+    def balance(self):
+        return self._balance
