24da3c5ae9aaacce697501e4e2ec0361241ec908
Christian <christian_pasquali@yahoo.com>
2026-07-09 16:22:34 -0400

    refactor yell function to use map for uppercase conversion

--- cs50/cs50_yell.py
+++ cs50/cs50_yell.py
@@ -3,12 +3,9 @@ def main():
 
 
 def yell(*words):
-    uppercased = []
-    for word in words:
-        uppercased.append(word.upper())
+    uppercased = map(str.upper, words)
     print(*uppercased)
         
-
 
 if __name__ == "__main__":
     main()
