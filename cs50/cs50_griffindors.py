b5c275e8ce23946a892c0b0b0585ffc75310ec62
Christian <christian_pasquali@yahoo.com>
2026-07-11 14:08:57 -0400

    update loop to print index and party leader from list

--- cs50/cs50_griffindors.py
+++ cs50/cs50_griffindors.py
@@ -1,4 +1,4 @@
 party_leaders = ["Carney", "Poilievre", "Blanchet", "Lewis", "May"]
 
-for party_leader in party_leaders:
-    print(party_leader)
+for i in range(len(party_leaders)):
+    print(i, party_leaders[i])
