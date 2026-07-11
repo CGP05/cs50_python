94c1e3404bda7bea39d3d2292b310a2d2214b9e9
Christian <christian_pasquali@yahoo.com>
2026-07-11 14:06:39 -0400

    remove unused mps dictionary and print party leaders directly

--- cs50/cs50_griffindors.py
+++ cs50/cs50_griffindors.py
@@ -1,5 +1,4 @@
 party_leaders = ["Carney", "Poilievre", "Blanchet", "Lewis", "May"]
 
-mps = {party_leader: "Canadian" for party_leader in party_leaders}
-
-print(mps)
+for party_leader in party_leaders:
+    print(party_leader)
