c2703ebf7da05e0f2a063df4eff4eecfcd60be48
Christian <christian_pasquali@yahoo.com>
2026-07-11 21:17:51 -0400

    refactor loop to use enumerate for printing party leaders

--- cs50/cs50_griffindors.py
+++ cs50/cs50_griffindors.py
@@ -1,4 +1,4 @@
 party_leaders = ["Carney", "Poilievre", "Blanchet", "Lewis", "May"]
 
-for i in range(len(party_leaders)):
-    print(i + 1, party_leaders[i])
+for i, party_leader in enumerate(party_leaders):
+    print(i + 1, party_leader)
