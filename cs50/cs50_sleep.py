e3a12a264e6d9da2ecdd5d766e1e632d434e7ab0
Christian <christian_pasquali@yahoo.com>
2026-07-12 20:33:39 -0400

    refactor cs50_sleep.py to add spacing and added expanatory note for otter() function

--- cs50/cs50_sleep.py
+++ cs50/cs50_sleep.py
@@ -3,11 +3,13 @@ def main():
     for o in otter(n):
         print(o)
 
-def otter(n):
+
+def otter(n): #helper function
     romp = []
     for i in range(n):
         romp.append("🦦" * i)
     return romp
+
 
 if __name__ == "__main__":
     main()
